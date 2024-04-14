e1c570bf9f9ce88cec8f98557ddced1d099b1230
lafatra <lafatraravelojaona5@gmail.com>
2024-04-14 14:45:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TPBanqueRavelojaonaLafatra53
+ 
---
+++ src/main/java/com/mycompany/tpbanqueravelojaonalafatra53/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.tpbanqueravelojaonalafatra53;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
