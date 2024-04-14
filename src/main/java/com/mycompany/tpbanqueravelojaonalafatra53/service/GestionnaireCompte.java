7aab83ad1da30e331c5f3c35bf66b674118002b5
lafatra <lafatraravelojaona5@gmail.com>
2024-04-14 15:37:59 +0200

    création des fonctions `creerCompte` et `getAllComptes`

--- src/main/java/com/mycompany/tpbanqueravelojaonalafatra53/service/GestionnaireCompte.java
+++ src/main/java/com/mycompany/tpbanqueravelojaonalafatra53/service/GestionnaireCompte.java
@@ -4,36 +4,55 @@
  */
 package com.mycompany.tpbanqueravelojaonalafatra53.service;
 
+import com.mycompany.tpbanqueravelojaonalafatra53.entity.CompteBancaire;
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
 
 /**
  *
  * @author HP
  */
 @Named(value = "gestionnaireCompte")
-@DataSourceDefinition (
-    className="com.mysql.cj.jdbc.MysqlDataSource",
-    name="java:app/jdbc/banque",
-    serverName="localhost",
-    portNumber=3306,
-    user="root",             
-    password="MySQL123",
-    databaseName="banque",
-    properties = {
-      "useSSL=false",
-      "allowPublicKeyRetrieval=true",
-      "driverClass=com.mysql.cj.jdbc.Driver"
-    }
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root",
+        password = "MySQL123",
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
 )
 @RequestScoped
 public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery<CompteBancaire> query = em.createQuery("SELECT c FROM CompteBancaire c", CompteBancaire.class);
+        return query.getResultList();
+    }
 
     /**
      * Creates a new instance of GestionnaireCompte
      */
     public GestionnaireCompte() {
     }
-    
+
 }
